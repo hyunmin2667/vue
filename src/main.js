0d79688ea563049fa2644ae0325e765838a97ac5
HYEONMIN LEE <hyunmin2667@gmail.com>
2023-06-08 01:56:36 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import Vue from "vue";
+import { createApp } from 'vue';
+import App from './App';
+import router from './routes/index.js';
+import store from './store/index.js';
 
-import './assets/main.css'
-
-createApp(App).mount('#app')
+createApp(App).use(router).use(store).mount('#app');
