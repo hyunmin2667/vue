91af6d989af5cd874cf185c4f7b64f4431b5ac07
HYEONMIN LEE <hyunmin2667@gmail.com>
2023-06-08 04:52:14 +0900

    design

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,93 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
   <header>
+    <h1>DSU BUS INFO</h1>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <section>
+      <h1>목적지를 선택하세요</h1>
+
+      <div class="segmented-controls">
+        <input id="three-1" name="three" type="radio" checked>
+        <label for="three-1">민석도서관</label>
+        <input id="three-2" name="three" type="radio">
+        <label for="three-2">스튜던트플라자</label>
+        <input id="three-3" name="three" type="radio">
+        <label for="three-3">글로벌빌리지</label>
+      </div>
+    </section>
+
+
+
+
   </header>
 
   <main>
-    <TheWelcome />
+
+    <ul class="post-list">
+      <li>
+        <span class="post-title">버스 1</span>
+        <span class="post-view">1분</span>
+      </li>
+      <li>
+        <span class="post-title">버스 2</span>
+        <span class="post-view">5분</span>
+      </li>
+      <li>
+        <span class="post-title">버스 3</span>
+        <span class="post-view">10분</span>
+      </li>
+      <!-- 추가적인 리스트 아이템을 여기에 추가할 수 있습니다 -->
+    </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    line-height: 1.5;
   }
 
   .logo {
-    margin: 0 2rem 0 0;
+    display: block;
+    margin: 0 auto 2rem;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  @media (min-width: 1024px) {
+    header {
+      display: flex;
+      place-items: center;
+      padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
   }
-}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,9 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      This is Test Page - LEE HYEON MIN <br>
+      <a target="_blank" href="https://vitejs.dev/">one</a> +
+      <a target="_blank" href="https://vuejs.org/">two</a>.
     </h3>
   </div>
 </template>
